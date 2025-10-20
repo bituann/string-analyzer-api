22bb96641154635f16aee13dae23fb9d6c01f79b
bituan <113036994+bituann@users.noreply.github.com>
2025-10-20 16:49:57 +0100

    Add request model

---
+++ src/main/java/com/bituan/string_analyzer_api/model/RequestModel.java
@@ -0,0 +1,13 @@
+package com.bituan.string_analyzer_api.model;
+
+public class RequestModel {
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
