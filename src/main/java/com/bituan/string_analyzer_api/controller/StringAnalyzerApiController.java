06979e8d80b8fc9c8ce234619d078d86747940b1
bituan <113036994+bituann@users.noreply.github.com>
2025-10-20 13:59:00 +0100

    Add constructor and service injection

--- src/main/java/com/bituan/string_analyzer_api/controller/StringAnalyzerApiController.java
+++ src/main/java/com/bituan/string_analyzer_api/controller/StringAnalyzerApiController.java
@@ -1,8 +1,15 @@
 package com.bituan.string_analyzer_api.controller;
 
+import com.bituan.string_analyzer_api.service.StringAnalyzerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StringAnalyzerApiController {
+    private final StringAnalyzerService stringAnalyzerService;
 
+    @Autowired
+    public StringAnalyzerApiController (StringAnalyzerService stringAnalyzerService) {
+        this.stringAnalyzerService = stringAnalyzerService;
+    }
 }
