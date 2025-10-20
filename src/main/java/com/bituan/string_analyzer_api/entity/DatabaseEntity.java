04c09e854a55d8f1f23e5b385a43e0b53a2f51e7
bituan <113036994+bituann@users.noreply.github.com>
2025-10-20 23:01:13 +0100

    Add method to get strings by filter

--- src/main/java/com/bituan/string_analyzer_api/entity/DatabaseEntity.java
+++ src/main/java/com/bituan/string_analyzer_api/entity/DatabaseEntity.java
@@ -3,8 +3,11 @@ package com.bituan.string_analyzer_api.entity;
 import com.bituan.string_analyzer_api.model.StringPropertiesModel;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class DatabaseEntity {
@@ -28,5 +31,28 @@ public class DatabaseEntity {
 
     public void removeString (String string) {
         database.remove(string);
+    }
+
+    public List<StringPropertiesModel> getStringsByFilter (Boolean isPalindrome, Integer minLength, Integer maxLength,
+                                                                        Integer wordCount, String containsCharacter) {
+        List<StringPropertiesModel> result = (List<StringPropertiesModel>) database.values();
+
+        if (isPalindrome != null) {
+            result = result.stream().filter(string -> string.getIs_palindrome() == isPalindrome).collect(Collectors.toList());
+        }
+        if (minLength != null) {
+            result = result.stream().filter(string -> string.getLength() >= minLength).collect(Collectors.toList());
+        }
+        if (maxLength != null) {
+            result = result.stream().filter(string -> string.getLength() <= maxLength).collect(Collectors.toList());
+        }
+        if (wordCount != null) {
+            result = result.stream().filter(string -> string.getWord_count() == wordCount).collect(Collectors.toList());
+        }
+        if (containsCharacter != null) {
+            result = result.stream().filter(string -> string.getCharcater_frequency_map().containsKey(containsCharacter)).collect(Collectors.toList());
+        }
+
+        return result;
     }
 }
