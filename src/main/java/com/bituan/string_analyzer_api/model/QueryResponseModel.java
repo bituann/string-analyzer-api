e5ae2295233181f666153a71a13eb379e5f7e81c
bituan <113036994+bituann@users.noreply.github.com>
2025-10-21 00:04:27 +0100

    Add response model for GET request with query filter

---
+++ src/main/java/com/bituan/string_analyzer_api/model/QueryResponseModel.java
@@ -0,0 +1,42 @@
+package com.bituan.string_analyzer_api.model;
+
+import java.util.List;
+import java.util.Map;
+
+public class QueryResponseModel {
+    private List<StringPropertiesModel> data;
+    private int count;
+    private Map<String, ?> filters_applied;
+
+    public QueryResponseModel() {}
+
+    public QueryResponseModel(List<StringPropertiesModel> data, int count, Map<String, ?> filters_applied) {
+        this.data = data;
+        this.count = count;
+        this.filters_applied = filters_applied;
+    }
+
+    public List<StringPropertiesModel> getData() {
+        return data;
+    }
+
+    public void setData(List<StringPropertiesModel> data) {
+        this.data = data;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public Map<String, ?> getFilters_applied() {
+        return filters_applied;
+    }
+
+    public void setFilters_applied(Map<String, ?> filters_applied) {
+        this.filters_applied = filters_applied;
+    }
+}
