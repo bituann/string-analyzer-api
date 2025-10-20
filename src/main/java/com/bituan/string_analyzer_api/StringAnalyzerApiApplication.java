919b97caa65158374b57b3fbbc3bc85bdc8b8fd0
bituan <113036994+bituann@users.noreply.github.com>
2025-10-20 11:30:48 +0100

    Initial Setup

---
+++ src/main/java/com/bituan/string_analyzer_api/StringAnalyzerApiApplication.java
@@ -0,0 +1,13 @@
+package com.bituan.string_analyzer_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringAnalyzerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringAnalyzerApiApplication.class, args);
+	}
+
+}
