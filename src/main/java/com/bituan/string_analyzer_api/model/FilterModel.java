4a598f0e2619b2b15b5925643970a70c897dbc3d
bituan <113036994+bituann@users.noreply.github.com>
2025-10-21 19:49:46 +0100

    Add annotation to exclude null props from json

--- src/main/java/com/bituan/string_analyzer_api/model/FilterModel.java
+++ src/main/java/com/bituan/string_analyzer_api/model/FilterModel.java
@@ -1,5 +1,8 @@
 package com.bituan.string_analyzer_api.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class FilterModel {
     private Boolean is_palindrome;
     private Integer min_length;
