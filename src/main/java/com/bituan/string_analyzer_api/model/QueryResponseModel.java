6eb6521ce793579f3426b818a6ae80226cdb90c1
bituan <113036994+bituann@users.noreply.github.com>
2025-10-21 01:40:57 +0100

    Change type of data property

--- src/main/java/com/bituan/string_analyzer_api/model/QueryResponseModel.java
+++ src/main/java/com/bituan/string_analyzer_api/model/QueryResponseModel.java
@@ -4,23 +4,23 @@ import java.util.List;
 import java.util.Map;
 
 public class QueryResponseModel {
-    private List<StringPropertiesModel> data;
+    private List<ResponseModel> data;
     private int count;
     private Map<String, ?> filters_applied;
 
     public QueryResponseModel() {}
 
-    public QueryResponseModel(List<StringPropertiesModel> data, int count, Map<String, ?> filters_applied) {
+    public QueryResponseModel(List<ResponseModel> data, int count, Map<String, ?> filters_applied) {
         this.data = data;
         this.count = count;
         this.filters_applied = filters_applied;
     }
 
-    public List<StringPropertiesModel> getData() {
+    public List<ResponseModel> getData() {
         return data;
     }
 
-    public void setData(List<StringPropertiesModel> data) {
+    public void setData(List<ResponseModel> data) {
         this.data = data;
     }
 
